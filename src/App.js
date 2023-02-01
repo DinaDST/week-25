dab008cd3d99deb8aee4c80347f1333d6f765577
DinaDST <for_work_Di@mail.ru>
2023-02-01 20:43:30 +0200

    add heroes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
+
 import "./App.css";
-import { Chart } from "react-google-charts";
+import "./components/hero.css";
 
-export const data = [
-    ["auto", "amount (in hundreds)"],
-    ["Lada", 117],
-    ["Geely", 74],
-    ["Volkswagen", 47],
-    ["Renault", 42],
-    ["Kia", 39],
-    ["GAZ", 29],
-    ["Huyndai", 25],
-    ["Nissan", 17],
-    ["УАЗ", 14],
-    ["Škoda", 12],
-    ["Toyota", 11],
-    ["BMW, Audi, Mercedes", 13],
-    ["other", 33],
-];
-
-export const options = {
-    title: "Car market in Belarus in 2022",
-    legend: "none",
-    pieSliceText: "label",
-    slices: {
-        4: { offset: 0.2 },
-        12: { offset: 0.3 },
-        14: { offset: 0.4 },
-        15: { offset: 0.5 },
-    },
-};
-
-export function App() {
-    return <Chart chartType = "PieChart"
-    data = { data }
-    options = { options }
-    width = { "100%" }
-    height = { "400px" }
-    />;
+class App extends React.Component {
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            div className = "hero-card" >
+            <
+            img src = "./bet.jpg"
+            alt = "batman" > { " " } <
+            /img>{" "} <
+            div className = "hero-name" > Бэтмэн < /div> <div className="hero-info"> Летучая мышь </div > { " " } <
+            /div>{" "} <
+            /div>
+        );
+    }
 }
 
 export default App;
